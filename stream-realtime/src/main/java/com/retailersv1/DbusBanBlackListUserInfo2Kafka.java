d82f3d7faea0a92104a6b440985dc089ecbfcc92
zhouhan <zhouhanker@gmail.com>
2025-05-07 14:53:29 +0800

    add banList docs

--- stream-realtime/src/main/java/com/retailersv1/DbusBanBlackListUserInfo2Kafka.java
+++ stream-realtime/src/main/java/com/retailersv1/DbusBanBlackListUserInfo2Kafka.java
@@ -52,11 +52,14 @@ public class DbusBanBlackListUserInfo2Kafka {
 
 
 
+        // {"info_original_total_amount":"24038.00","info_activity_reduce_amount":"0.00","commentTxt":"\n\n小米电视4A屏幕显示效果一般，建议谨慎购买。","info_province_id":14,"info_payment_way":"3501","ds":"20250506","info_create_time":1746543508000,"info_refundable_time":1747148308000,"info_order_status":"1001","id":68,"spu_id":6,"table":"comment_info","info_tm_ms":1746518018698,"op":"c","create_time":1746543560000,"info_user_id":26,"info_op":"c","info_trade_body":"小米电视4A 70英寸 4K超高清 HDR 二级能效 2GB+16GB L70M5-4A 内置小爱 智能网络液晶平板教育电视等8件商品","sku_id":21,"server_id":"1","dic_name":"好评","info_consignee_tel":"13477763374","info_total_amount":"23968.00","info_out_trade_no":"443513674664624","appraise":"1201","user_id":26,"info_id":836,"info_coupon_reduce_amount":"70.00","order_id":836,"info_consignee":"穆素云","ts_ms":1746518019174,"db":"realtime_v1"}
         SingleOutputStreamOperator<JSONObject> mapJsonStr = kafkaCdcDbSource.map(JSON::parseObject).uid("to_json_string").name("to_json_string");
+
 
         SingleOutputStreamOperator<JSONObject> bloomFilterDs = mapJsonStr.keyBy(data -> data.getLong("order_id"))
                 .filter(new FilterBloomDeduplicatorFunc(1000000, 0.01));
 
+        // {"msg":"\n\nTCL 85Q6电视效果与价格不匹配！,西毒是共党","consignee":"臧宁欣","violation_grade":"P0","user_id":221,"violation_msg":"西毒是共党","is_violation":1,"ts_ms":1746596800252,"ds":"20250507"}
         SingleOutputStreamOperator<JSONObject> SensitiveWordsDs = bloomFilterDs.map(new MapCheckRedisSensitiveWordsFunc())
                 .uid("MapCheckRedisSensitiveWord")
                 .name("MapCheckRedisSensitiveWord");
@@ -77,8 +80,6 @@ public class DbusBanBlackListUserInfo2Kafka {
                 return jsonObject;
             }
         }).uid("second sensitive word check").name("second sensitive word check");
-
-        secondCheckMap.print();
 
         secondCheckMap.map(data -> data.toJSONString())
                         .sinkTo(
--- stream-realtime/src/main/java/com/retailersv1/DbusDBCommentFactData2Kafka.java
+++ stream-realtime/src/main/java/com/retailersv1/DbusDBCommentFactData2Kafka.java
@@ -84,17 +84,20 @@ public class DbusDBCommentFactData2Kafka {
         ).uid("kafka_cdc_db_source").name("kafka_cdc_db_source");
 
         // 订单主表
+        // {"op":"c","after":{"payment_way":"3501","consignee":"窦先敬","create_time":1746660084000,"refundable_time":1747264884000,"original_total_amount":"6499.00","coupon_reduce_amount":"0.00","order_status":"1001","out_trade_no":"929361421194788","total_amount":"5999.00","user_id":184,"province_id":23,"consignee_tel":"13879332785","trade_body":"小米12S Ultra 骁龙8+旗舰处理器 徕卡光学镜头 2K超视感屏 120Hz高刷 67W快充 12GB+256GB 经典黑 5G手机等1件商品","id":1123,"activity_reduce_amount":"500.00"},"source":{"file":"mysql-bin.000004","connector":"mysql","pos":31381479,"name":"mysql_binlog_source","thread":20265,"row":0,"server_id":1,"version":"1.9.7.Final","ts_ms":1746596800000,"snapshot":"false","db":"realtime_v1","table":"order_info"},"ts_ms":1746596800483}
         DataStream<JSONObject> filteredOrderInfoStream = kafkaCdcDbSource
                 .map(JSON::parseObject)
                 .filter(json -> json.getJSONObject("source").getString("table").equals("order_info"))
                 .uid("kafka_cdc_db_order_source").name("kafka_cdc_db_order_source");
 
         // 评论表进行进行升维处理 和hbase的维度进行关联补充维度数据
+        // {"op":"c","after":{"create_time":1746624077000,"user_id":178,"appraise":"1201","comment_txt":"评论内容：44237268662145286925725839461514467765118653811952","nick_name":"珠珠","sku_id":14,"id":85,"spu_id":4,"order_id":1010},"source":{"file":"mysql-bin.000004","connector":"mysql","pos":30637591,"name":"mysql_binlog_source","thread":20256,"row":0,"server_id":1,"version":"1.9.7.Final","ts_ms":1746596796000,"snapshot":"false","db":"realtime_v1","table":"comment_info"},"ts_ms":1746596796319}
         DataStream<JSONObject> filteredStream = kafkaCdcDbSource
                 .map(JSON::parseObject)
                 .filter(json -> json.getJSONObject("source").getString("table").equals("comment_info"))
                 .keyBy(json -> json.getJSONObject("after").getString("appraise"));
 
+        // {"op":"c","after":{"create_time":1746568494000,"user_id":126,"appraise":"1202","comment_txt":"评论内容：43341158654483726916799957869464279782846343359228","nick_name":"琬琬","sku_id":5,"id":77,"spu_id":2,"order_id":334,"dic_name":"中评"},"source":{"file":"mysql-bin.000004","connector":"mysql","pos":29984187,"name":"mysql_binlog_source","thread":19913,"row":0,"server_id":1,"version":"1.9.7.Final","ts_ms":1746518022000,"snapshot":"false","db":"realtime_v1","table":"comment_info"},"ts_ms":1746518022747}
         DataStream<JSONObject> enrichedStream = AsyncDataStream
                 .unorderedWait(
                         filteredStream,
@@ -105,6 +108,8 @@ public class DbusDBCommentFactData2Kafka {
                 ).uid("async_hbase_dim_base_dic_func")
                 .name("async_hbase_dim_base_dic_func");
 
+
+        // {"op":"c","create_time":1746653124000,"commentTxt":"评论内容：36913887965764674188858298813931966419435136341364","sku_id":19,"server_id":"1","dic_name":"好评","appraise":"1201","user_id":221,"id":89,"spu_id":5,"order_id":979,"ts_ms":1746596800251,"db":"realtime_v1","table":"comment_info"}
         SingleOutputStreamOperator<JSONObject> orderCommentMap = enrichedStream.map(new RichMapFunction<JSONObject, JSONObject>() {
                     @Override
                     public JSONObject map(JSONObject jsonObject){
@@ -139,6 +144,8 @@ public class DbusDBCommentFactData2Kafka {
                 .uid("map-order_comment_data")
                 .name("map-order_comment_data");
 
+
+        // {"op":"c","payment_way":"3501","consignee":"张贞","create_time":1746653800000,"refundable_time":1747258600000,"original_total_amount":"69.00","coupon_reduce_amount":"0.00","order_status":"1001","out_trade_no":"914927687659481","total_amount":"69.00","user_id":156,"province_id":10,"tm_ms":1746596799810,"consignee_tel":"13114791128","trade_body":"CAREMiLLE珂曼奶油小方口红 雾面滋润保湿持久丝缎唇膏 M01醉蔷薇等1件商品","id":1108,"activity_reduce_amount":"0.00"}
         SingleOutputStreamOperator<JSONObject> orderInfoMapDs = filteredOrderInfoStream.map(new RichMapFunction<JSONObject, JSONObject>() {
             @Override
             public JSONObject map(JSONObject inputJsonObj){
@@ -158,15 +165,20 @@ public class DbusDBCommentFactData2Kafka {
             }
         }).uid("map-order_info_data").name("map-order_info_data");
 
+
         // orderCommentMap.order_id join orderInfoMapDs.id
         KeyedStream<JSONObject, String> keyedOrderCommentStream = orderCommentMap.keyBy(data -> data.getString("order_id"));
         KeyedStream<JSONObject, String> keyedOrderInfoStream = orderInfoMapDs.keyBy(data -> data.getString("id"));
 
+        // {"info_original_total_amount":"56092.00","info_activity_reduce_amount":"1199.90","commentTxt":"评论内容：52198813817222113474133821791377912858419193882331","info_province_id":8,"info_payment_way":"3501","info_create_time":1746624020000,"info_refundable_time":1747228820000,"info_order_status":"1002","id":84,"spu_id":3,"table":"comment_info","info_tm_ms":1746596796189,"info_operate_time":1746624052000,"op":"c","create_time":1746624077000,"info_user_id":178,"info_op":"u","info_trade_body":"Apple iPhone 12 (A2404) 64GB 白色 支持移动联通电信5G 双卡双待手机等6件商品","sku_id":11,"server_id":"1","dic_name":"好评","info_consignee_tel":"13316189177","info_total_amount":"54892.10","info_out_trade_no":"692358523797933","appraise":"1201","user_id":178,"info_id":1010,"info_coupon_reduce_amount":"0.00","order_id":1010,"info_consignee":"彭永","ts_ms":1746596796318,"db":"realtime_v1"}
         SingleOutputStreamOperator<JSONObject> orderMsgAllDs = keyedOrderCommentStream.intervalJoin(keyedOrderInfoStream)
                 .between(Time.minutes(-1), Time.minutes(1))
                 .process(new IntervalJoinOrderCommentAndOrderInfoFunc())
                 .uid("interval_join_order_comment_and_order_info_func").name("interval_join_order_comment_and_order_info_func");
 
+
+        // 通过AI 生成评论数据，`Deepseek 7B` 模型即可
+        // {"info_original_total_amount":"1299.00","info_activity_reduce_amount":"0.00","commentTxt":"\n\n这款Redmi 10X虽然价格亲民，但续航能力一般且相机效果平平，在同类产品中竞争力不足。","info_province_id":32,"info_payment_way":"3501","info_create_time":1746566254000,"info_refundable_time":1747171054000,"info_order_status":"1004","id":75,"spu_id":2,"table":"comment_info","info_tm_ms":1746518021300,"info_operate_time":1746563573000,"op":"c","create_time":1746563573000,"info_user_id":149,"info_op":"u","info_trade_body":"Redmi 10X 4G Helio G85游戏芯 4800万超清四摄 5020mAh大电量 小孔全面屏 128GB大存储 8GB+128GB 明月灰 游戏智能手机 小米 红米等1件商品","sku_id":7,"server_id":"1","dic_name":"好评","info_consignee_tel":"13144335624","info_total_amount":"1299.00","info_out_trade_no":"199223184973112","appraise":"1201","user_id":149,"info_id":327,"info_coupon_reduce_amount":"0.00","order_id":327,"info_consignee":"范琳","ts_ms":1746518021294,"db":"realtime_v1"}
         SingleOutputStreamOperator<JSONObject> supplementDataMap = orderMsgAllDs.map(new RichMapFunction<JSONObject, JSONObject>() {
             @Override
             public JSONObject map(JSONObject jsonObject) {
@@ -175,6 +187,8 @@ public class DbusDBCommentFactData2Kafka {
             }
         }).uid("map-generate_comment").name("map-generate_comment");
 
+
+        // {"info_original_total_amount":"56092.00","info_activity_reduce_amount":"1199.90","commentTxt":"\n\n差评：续航差、相机效果一般,高自联","info_province_id":8,"info_payment_way":"3501","info_create_time":1746624020000,"info_refundable_time":1747228820000,"info_order_status":"1001","id":88,"spu_id":5,"table":"comment_info","info_tm_ms":1746596795948,"op":"c","create_time":1746624077000,"info_user_id":178,"info_op":"c","info_trade_body":"Apple iPhone 12 (A2404) 64GB 白色 支持移动联通电信5G 双卡双待手机等6件商品","sku_id":19,"server_id":"1","dic_name":"好评","info_consignee_tel":"13316189177","info_total_amount":"54892.10","info_out_trade_no":"692358523797933","appraise":"1201","user_id":178,"info_id":1010,"info_coupon_reduce_amount":"0.00","order_id":1010,"info_consignee":"彭永","ts_ms":1746596796327,"db":"realtime_v1"}
         SingleOutputStreamOperator<JSONObject> suppleMapDs = supplementDataMap.map(new RichMapFunction<JSONObject, JSONObject>() {
             private transient Random random;
 
