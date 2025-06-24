b34922fd289a0d49fa4dbfafff1f4b3438ec8fd2
zhouhan <zhouhanker@gmail.com>
2025-06-24 09:26:53 +0800

    add seatunnel submit local shell

--- script/seatunnel-conf/seatunnel_commod_submit.sh
+++ script/seatunnel-conf/seatunnel_commod_submit.sh
@@ -9,3 +9,6 @@
 /opt/soft/seatunnel-2.3.8/bin/seatunnel.sh -c /opt/soft/seatunnel-2.3.8/user_config/mysql_to_print.conf \
 -i mysql.host='10.39.48.36' \
 -m local
+
+/opt/soft/seatunnel-2.3.8/bin/seatunnel.sh -c /opt/soft/seatunnel-2.3.8/user_config/mysql_to_print.conf \
+-m local
